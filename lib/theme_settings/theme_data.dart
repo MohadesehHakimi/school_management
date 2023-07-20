23af70bc00a0bb75e9aa050640171fb00ce2cd8e
mohadeseh <hakimi.d.f.25@gmail.com>
2023-07-20 18:38:27 +0330

    chore: add titleMedium theme to theme data

--- lib/theme_settings/theme_data.dart
+++ lib/theme_settings/theme_data.dart
@@ -13,6 +13,10 @@ ThemeData preferredThemeData() {
         fontSize: 24.0,
         color: Colors.white,
       ),
+      titleMedium: TextStyle(
+        fontSize: 20.0,
+        color: Colors.white,
+      ),
       titleSmall: TextStyle(
         fontSize: 16.0,
         color: Colors.white,
