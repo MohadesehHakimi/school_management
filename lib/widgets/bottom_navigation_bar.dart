61293c8522479d11bd51951206ea295fe30f3a96
mohadeseh <hakimi.d.f.25@gmail.com>
2023-07-20 18:39:28 +0330

    chore: increase height of bottom navigation bar

--- lib/widgets/bottom_navigation_bar.dart
+++ lib/widgets/bottom_navigation_bar.dart
@@ -19,38 +19,41 @@ class _CustomBottomNavigationBarState extends State<CustomBottomNavigationBar> {
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: const BorderRadius.only(
-        topLeft: Radius.circular(25.0),
-        topRight: Radius.circular(25.0),
-      ),
-      child: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: FaIcon(FontAwesomeIcons.house),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: FaIcon(FontAwesomeIcons.message),
-            label: 'Messages',
-          ),
-          BottomNavigationBarItem(
-            icon: FaIcon(FontAwesomeIcons.bell),
-            label: 'Notifications',
-          ),
-          BottomNavigationBarItem(
-            icon: FaIcon(FontAwesomeIcons.user),
-            label: 'Profile',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.white,
-        unselectedItemColor: Colors.white.withOpacity(0.5),
-        selectedFontSize: 16.0,
-        unselectedFontSize: 14.0,
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        onTap: _onItemTapped,
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.08,
+      child: ClipRRect(
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(25.0),
+          topRight: Radius.circular(25.0),
+        ),
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: FaIcon(FontAwesomeIcons.house),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: FaIcon(FontAwesomeIcons.message),
+              label: 'Messages',
+            ),
+            BottomNavigationBarItem(
+              icon: FaIcon(FontAwesomeIcons.bell),
+              label: 'Notifications',
+            ),
+            BottomNavigationBarItem(
+              icon: FaIcon(FontAwesomeIcons.user),
+              label: 'Profile',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.white.withOpacity(0.5),
+          selectedFontSize: 16.0,
+          unselectedFontSize: 14.0,
+          backgroundColor: Theme.of(context).colorScheme.primary,
+          onTap: _onItemTapped,
+        ),
       ),
     );
   }
