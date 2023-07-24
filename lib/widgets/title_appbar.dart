d9338f996a8164f0dfb333447384340d76afeac8
mohadeseh <hakimi.d.f.25@gmail.com>
2023-07-24 23:04:21 +0330

    feat: create title appbar layout

---
+++ lib/widgets/title_appbar.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class TitleAppBar extends StatelessWidget {
+  const TitleAppBar({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(
+        top: 50.0,
+        left: 20.0,
+        right: 20.0,
+        bottom: 20.0,
+      ),
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Theme.of(context).colorScheme.secondary,
+              Theme.of(context).colorScheme.primary,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          )
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              IconButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                icon: const FaIcon(
+                  FontAwesomeIcons.arrowLeft,
+                  color: Colors.white,
+                  size: 20.0,
+                ),
+              ),
+              Text(
+                title,
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                child: IconButton(
+                  onPressed: () {},
+                  icon: const FaIcon(FontAwesomeIcons.bell),
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+              ),
+              const SizedBox(width: 10.0,),
+              IconButton(
+                onPressed: () {},
+                icon: const FaIcon(FontAwesomeIcons.bars),
+                color: Colors.white,
+              ),
+            ],
+          )
+        ],
+      )
+    );
+  }
+}
