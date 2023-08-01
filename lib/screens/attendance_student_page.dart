8052dda5d38d5a94d4f40bc1f2bb89849eea88ce
mohadeseh <hakimi.d.f.25@gmail.com>
2023-08-02 00:17:50 +0330

    chore: change some styling

--- lib/screens/attendance_student_page.dart
+++ lib/screens/attendance_student_page.dart
@@ -18,176 +18,172 @@ class AttendanceStudentPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        FocusManager.instance.primaryFocus?.unfocus();
-      },
-      child: Scaffold(
-        body: Stack(
-          fit: StackFit.expand,
-          children: [
-            PreferredSize(
-              preferredSize: Size.fromHeight(MediaQuery.of(context).size.height * 0.1),
-              child: const TitleAppBar(title: 'Attendance'),
-            ),
-            Positioned(
-              top: MediaQuery.of(context).size.height * 0.15,
-              left: 0.0,
-              right: 0.0,
-              bottom: 0.0,
-              child: Container(
-                decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(25.0),
-                    topRight: Radius.circular(25.0),
-                  ),
-                  color: Colors.white,
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          PreferredSize(
+            preferredSize: Size.fromHeight(MediaQuery.of(context).size.height * 0.1),
+            child: const TitleAppBar(title: 'Attendance'),
+          ),
+          Positioned(
+            top: MediaQuery.of(context).size.height * 0.15,
+            left: 0.0,
+            right: 0.0,
+            bottom: 0.0,
+            child: Container(
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(25.0),
+                  topRight: Radius.circular(25.0),
                 ),
-                padding: const EdgeInsets.only(
-                  top: 45.0,
-                  left: 20.0,
-                  right: 20.0,
-                ),
-                child: SingleChildScrollView(
-                  physics: const BouncingScrollPhysics(),
-                  child: Column(
-                    children: [
-                      ListView.separated(
-                        physics: const NeverScrollableScrollPhysics(),
-                        shrinkWrap: true,
-                        scrollDirection: Axis.vertical,
-                        padding: const EdgeInsets.all(0.0),
-                        itemCount: sampleAttendance.length,
-                        itemBuilder: (context, index) {
-                          return Column(
-                            children: [
-                              Text(
-                                sampleAttendance[index].keys.first,
-                                style: const TextStyle(
-                                  fontSize: 20.0,
-                                  fontWeight: FontWeight.bold,
-                                ),
+                color: Colors.white,
+              ),
+              padding: const EdgeInsets.only(
+                top: 45.0,
+                left: 20.0,
+                right: 20.0,
+              ),
+              child: SingleChildScrollView(
+                physics: const BouncingScrollPhysics(),
+                child: Column(
+                  children: [
+                    ListView.separated(
+                      physics: const NeverScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      scrollDirection: Axis.vertical,
+                      padding: const EdgeInsets.all(0.0),
+                      itemCount: sampleAttendance.length,
+                      itemBuilder: (context, index) {
+                        return Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              sampleAttendance[index].keys.first,
+                              style: const TextStyle(
+                                fontSize: 20.0,
                               ),
-                              const SizedBox(height: 10.0),
-                              SizedBox(
-                                height: MediaQuery.of(context).size.height * 0.25,
-                                child: Card(
-                                  elevation: 5.0,
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(15.0),
-                                  ),
-                                  child: Row(
-                                    children: [
-                                      SizedBox(
-                                        width: MediaQuery.of(context).size.width * 0.5,
-                                        child: SfCircularChart(
-                                          annotations: [
-                                            CircularChartAnnotation(
-                                              widget: Text(
-                                                '${sampleAttendance[index].values.first[0]}',
-                                                style: const TextStyle(
-                                                  fontSize: 24.0,
-                                                  fontWeight: FontWeight.bold,
-                                                ),
+                            ),
+                            const SizedBox(height: 10.0),
+                            SizedBox(
+                              height: MediaQuery.of(context).size.height * 0.23,
+                              child: Card(
+                                elevation: 5.0,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15.0),
+                                ),
+                                child: Row(
+                                  children: [
+                                    SizedBox(
+                                      width: MediaQuery.of(context).size.width * 0.5,
+                                      child: SfCircularChart(
+                                        annotations: [
+                                          CircularChartAnnotation(
+                                            widget: Text(
+                                              '${sampleAttendance[index].values.first[0]}',
+                                              style: const TextStyle(
+                                                fontSize: 24.0,
+                                                fontWeight: FontWeight.bold,
                                               ),
-                                              verticalAlignment: ChartAlignment.far,
                                             ),
-                                            CircularChartAnnotation(
-                                              widget: Text(
-                                                'Days Present',
-                                                style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                                                  color: Colors.grey[600],
-                                                )
-                                              ),
-                                              verticalAlignment: ChartAlignment.near,
-                                            ),
-                                          ],
-                                          palette: [
-                                            Theme.of(context).colorScheme.primary,
-                                            Colors.white,
-                                          ],
-                                          series: <CircularSeries>[
-                                            DoughnutSeries<double, double>(
-                                              dataSource: <double>[
-                                                sampleAttendance[index].values.first[0] /
-                                                    sampleAttendance[index].values.first[1] * 100,
-                                                100 -
-                                                    sampleAttendance[index].values.first[0] /
-                                                        sampleAttendance[index].values.first[1] * 100,
-                                              ],
-                                              xValueMapper: (double data, _) => data,
-                                              yValueMapper: (double data, _) => data,
-                                              radius: '80%',
-                                              innerRadius: '85%',
-                                              startAngle: 285,
-                                              endAngle: 285,
-                                              cornerStyle: CornerStyle.bothCurve,
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                      Column(
-                                        mainAxisAlignment: MainAxisAlignment.center,
-                                        children: [
-                                          Text(
-                                            'Total Working Days',
-                                            style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                                              color: Theme.of(context).colorScheme.primary,
-                                              fontWeight: FontWeight.bold,
-                                            )
+                                            verticalAlignment: ChartAlignment.far,
                                           ),
-                                          const SizedBox(height: 10.0),
-                                          Text(
-                                            '${sampleAttendance[index].values.first[1]}',
-                                            style: const TextStyle(
-                                              fontSize: 18.0,
-                                              fontWeight: FontWeight.bold,
+                                          CircularChartAnnotation(
+                                            widget: Text(
+                                              'Days Present',
+                                              style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                                                color: Colors.grey[600],
+                                              )
                                             ),
-                                          ),
-                                          const SizedBox(height: 10.0),
-                                          Text(
-                                            'Official Leaves',
-                                            style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                                              color: Theme.of(context).colorScheme.primary,
-                                              fontWeight: FontWeight.bold,
-                                            )
-                                          ),
-                                          const SizedBox(height: 10.0),
-                                          Text(
-                                            '${sampleAttendance[index].values.first[2]}',
-                                            style: const TextStyle(
-                                              fontSize: 18.0,
-                                              fontWeight: FontWeight.bold,
-                                            ),
+                                            verticalAlignment: ChartAlignment.near,
                                           ),
                                         ],
-                                      )
-                                    ],
-                                  ),
+                                        palette: [
+                                          Theme.of(context).colorScheme.primary,
+                                          Colors.white,
+                                        ],
+                                        series: <CircularSeries>[
+                                          DoughnutSeries<double, double>(
+                                            dataSource: <double>[
+                                              sampleAttendance[index].values.first[0] /
+                                                  sampleAttendance[index].values.first[1] * 100,
+                                              100 -
+                                                  sampleAttendance[index].values.first[0] /
+                                                      sampleAttendance[index].values.first[1] * 100,
+                                            ],
+                                            xValueMapper: (double data, _) => data,
+                                            yValueMapper: (double data, _) => data,
+                                            radius: '80%',
+                                            innerRadius: '85%',
+                                            startAngle: 285,
+                                            endAngle: 285,
+                                            cornerStyle: CornerStyle.bothCurve,
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                    Column(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          'Total Working Days',
+                                          style: TextStyle(
+                                            color: Theme.of(context).colorScheme.primary,
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 16.0,
+                                          )
+                                        ),
+                                        const SizedBox(height: 10.0),
+                                        Text(
+                                          '${sampleAttendance[index].values.first[1]} Days',
+                                          style: const TextStyle(
+                                            fontSize: 16.0,
+                                          ),
+                                        ),
+                                        const SizedBox(height: 20.0),
+                                        Text(
+                                          'Official Leaves',
+                                          style: TextStyle(
+                                            color: Theme.of(context).colorScheme.primary,
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 16.0,
+                                          )
+                                        ),
+                                        const SizedBox(height: 10.0),
+                                        Text(
+                                          '${sampleAttendance[index].values.first[2]} Days',
+                                          style: const TextStyle(
+                                            fontSize: 16.0,
+                                          ),
+                                        ),
+                                      ],
+                                    )
+                                  ],
                                 ),
-                              )
-                            ],
-                          );
-                        },
-                        separatorBuilder: (context, index) {
-                          return Divider(
-                            height: 50.0,
-                            thickness: 1.0,
-                            indent: 20.0,
-                            endIndent: 20.0,
-                            color: Colors.grey[300],
-                          );
-                        },
-                      ),
-                    ],
-                  )
-                ),
+                              ),
+                            )
+                          ],
+                        );
+                      },
+                      separatorBuilder: (context, index) {
+                        return Divider(
+                          height: 50.0,
+                          thickness: 1.0,
+                          indent: 20.0,
+                          endIndent: 20.0,
+                          color: Colors.grey[300],
+                        );
+                      },
+                    ),
+                  ],
+                )
               ),
             ),
-          ],
-        ),
-        bottomNavigationBar: const CustomBottomNavigationBar(),
+          ),
+        ],
       ),
+      bottomNavigationBar: const CustomBottomNavigationBar(),
     );
   }
 }
