a2b9f6c2716d826929a14f712f8ec00787492df3
mohadeseh <hakimi.d.f.25@gmail.com>
2023-08-11 20:14:48 +0330

    feat: improve styling of signup page

--- lib/screens/signup_page.dart
+++ lib/screens/signup_page.dart
@@ -20,96 +20,126 @@ class _SignUpPageState extends State<SignUpPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 50.0,
-              vertical: 15.0,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Phone Number',
-                  style: Theme.of(context).textTheme.bodyMedium,
-                ),
-                TextFormField(
-                  keyboardType: TextInputType.phone,
-                  style: TextStyle(
-                    color: Theme.of(context).colorScheme.primary,
+    return GestureDetector(
+      onTap: () {
+        FocusManager.instance.primaryFocus?.unfocus();
+      },
+      child: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 50.0,
+                vertical: 10.0,
+              ),
+              child: TextFormField(
+                keyboardType: TextInputType.name,
+                decoration: const InputDecoration(
+                  prefixIcon: FaIcon(
+                    FontAwesomeIcons.user,
+                  ),
+                  prefixIconConstraints: BoxConstraints(
+                    minWidth: 50.0,
+                  ),
+                  label: Text(
+                    'Full Name',
                   ),
                 ),
-              ],
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.primary,
+                )
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 50.0,
-              vertical: 15.0,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Password',
-                  style: Theme.of(context).textTheme.bodyMedium,
-                ),
-                TextFormField(
-                  style: TextStyle(
-                    color: Theme.of(context).colorScheme.primary,
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 50.0,
+                vertical: 10.0,
+              ),
+              child: TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(
+                  prefixIcon: FaIcon(
+                    FontAwesomeIcons.envelope,
                   ),
-                  obscureText: !_isPasswordVisible,
-                  decoration: InputDecoration(
-                    suffixIcon: IconButton(
-                      icon: FaIcon(
-                        _isPasswordVisible
-                            ? FontAwesomeIcons.eyeSlash
-                            : FontAwesomeIcons.eye,
-                      ),
-                      onPressed: _togglePasswordVisibility,
+                  prefixIconConstraints: BoxConstraints(
+                    minWidth: 50.0,
+                  ),
+                  label: Text(
+                    'Email',
+                  ),
+                ),
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.primary,
+                )
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 50.0,
+                vertical: 10.0,
+              ),
+              child: TextFormField(
+                obscureText: !_isPasswordVisible,
+                decoration: InputDecoration(
+                  prefixIcon: const FaIcon(
+                    FontAwesomeIcons.lock,
+                  ),
+                  prefixIconConstraints: const BoxConstraints(
+                    minWidth: 50.0,
+                  ),
+                  suffixIcon: IconButton(
+                    onPressed: _togglePasswordVisibility,
+                    icon: FaIcon(
+                      _isPasswordVisible
+                        ? FontAwesomeIcons.eyeSlash
+                        : FontAwesomeIcons.eye,
                     ),
                   ),
+                  label: const Text(
+                    'Password',
+                  ),
                 ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 80.0),
-            child: TextButton(
-              onPressed: () {},
-              style: TextButton.styleFrom(
-                backgroundColor: Theme.of(context).colorScheme.primary,
-                elevation: 5.0,
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 25.0,
-                  vertical: 10.0,
-                ),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-              ),
-              child: Text(
-                'Sign Up',
-                style: Theme.of(context).textTheme.titleMedium,
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.primary,
+                )
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 20.0),
-            child: GestureDetector(
-              child: Text(
-                'Already have an account?',
-                style: Theme.of(context).textTheme.bodySmall?.copyWith(
-                  decoration: TextDecoration.underline,
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  backgroundColor: Theme.of(context).colorScheme.primary,
+                  elevation: 5.0,
+                  padding: EdgeInsets.symmetric(
+                    horizontal: MediaQuery.of(context).size.width * 0.3,
+                    vertical: 10.0,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+                child: Text(
+                  'Sign Up',
+                  style: Theme.of(context).textTheme.titleMedium,
                 ),
               ),
             ),
-          ),
-          SizedBox(height: MediaQuery.of(context).size.height * 0.1,)
-        ],
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: GestureDetector(
+                child: Text(
+                  'Already have an account?',
+                  style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                    decoration: TextDecoration.underline,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.1,)
+          ],
+        ),
       ),
     );
   }
