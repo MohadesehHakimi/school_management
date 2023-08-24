50c07b06806bedba306c9d26eff4cbd79baebe48
mohadeseh <hakimi.d.f.25@gmail.com>
2023-08-24 16:47:51 +0330

    feat: add stats sample to teacher profile

--- lib/screens/teacher_profile.dart
+++ lib/screens/teacher_profile.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+import '../models/class.dart';
+
 class TeacherProfile extends StatefulWidget {
   const TeacherProfile({super.key, required this.user});
 
@@ -92,10 +94,12 @@ class _TeacherProfileState extends State<TeacherProfile> with SingleTickerProvid
               top: MediaQuery.of(context).size.height * 0.16,
               left: 20.0,
               right: 20.0,
+              bottom: 20.0,
             ),
             child: TabBarView(
               controller: _tabController,
               children: [
+                // Personal Info
                 SingleChildScrollView(
                   physics: const BouncingScrollPhysics(),
                   padding: const EdgeInsets.only(
@@ -208,9 +212,9 @@ class _TeacherProfileState extends State<TeacherProfile> with SingleTickerProvid
                                 leading: const FaIcon(FontAwesomeIcons.chalkboardUser),
                                 title: const Padding(
                                   padding: EdgeInsets.only(bottom: 8.0),
-                                  child: Text('Subject',),
+                                  child: Text('Subjects',),
                                 ),
-                                subtitle: const Text('Science'),
+                                subtitle: const Text('Science, English'),
                                 iconColor: Theme.of(context).colorScheme.primary,
                                 titleTextStyle: Theme.of(context).textTheme.bodyMedium!.copyWith(
                                   color: Colors.grey,
@@ -226,7 +230,7 @@ class _TeacherProfileState extends State<TeacherProfile> with SingleTickerProvid
                                   padding: EdgeInsets.only(bottom: 8.0),
                                   child: Text('Work Experience',),
                                 ),
-                                subtitle: const Text('5'),
+                                subtitle: const Text('5 Years'),
                                 iconColor: Theme.of(context).colorScheme.primary,
                                 titleTextStyle: Theme.of(context).textTheme.bodyMedium!.copyWith(
                                   color: Colors.grey,
@@ -259,8 +263,269 @@ class _TeacherProfileState extends State<TeacherProfile> with SingleTickerProvid
                     ),
                   ),
                 ),
-                const Center(
-                  child: Text('Stats'),
+                // Stats
+                SingleChildScrollView(
+                  physics: const BouncingScrollPhysics(),
+                  padding: const EdgeInsets.only(
+                    bottom: 20.0,
+                  ),
+                  child: Center(
+                    child: Column(
+                      children: [
+                        Card(
+                          elevation: 5.0,
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                          ),
+                          child: Container(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Column(
+                              children: [
+                                const Row(
+                                  children: [
+                                    Text(
+                                      'Performance Report',
+                                      style: TextStyle(
+                                        fontSize: 16.0,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    Spacer(),
+                                  ],
+                                ),
+                                const SizedBox(height: 20.0,),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    Column(
+                                      children: [
+                                        Text(
+                                          'Total Students',
+                                          style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                                            color: Colors.grey,
+                                          ),
+                                        ),
+                                        const SizedBox(height: 10.0,),
+                                        Text(
+                                          '50',
+                                          style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                      ]
+                                    ),
+                                    Column(
+                                      children: [
+                                        Text(
+                                          'Total Classes',
+                                          style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                                            color: Colors.grey,
+                                          ),
+                                        ),
+                                        const SizedBox(height: 10.0,),
+                                        Text(
+                                          '5',
+                                          style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                      ]
+                                    ),
+                                  ],
+                                ),
+                                const SizedBox(height: 20.0,),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    Column(
+                                      children: [
+                                        Text(
+                                          'Total Subjects',
+                                          style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                                            color: Colors.grey,
+                                          ),
+                                        ),
+                                        const SizedBox(height: 10.0,),
+                                        Text(
+                                          '2',
+                                          style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                      ]
+                                    ),
+                                    Column(
+                                      children: [
+                                        Text(
+                                          'Total Exams',
+                                          style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                                            color: Colors.grey,
+                                          ),
+                                        ),
+                                        const SizedBox(height: 10.0,),
+                                        Text(
+                                          '23',
+                                          style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                      ]
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          )
+                        ),
+                        const SizedBox(height: 20.0,),
+                        Card(
+                          elevation: 5.0,
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                          ),
+                          child: Container(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Column(
+                              children: [
+                                const Row(
+                                  children: [
+                                    Text(
+                                      'TimeTable',
+                                      style: TextStyle(
+                                        fontSize: 16.0,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    Spacer(),
+                                  ],
+                                ),
+                                ListView.builder(
+                                  shrinkWrap: true,
+                                  physics: const NeverScrollableScrollPhysics(),
+                                  padding: const EdgeInsets.only(top: 16.0),
+                                  itemCount: sampleClasses.length,
+                                  itemBuilder: (context, index) {
+                                    return ExpansionTile(
+                                      title: Text(
+                                        sampleClasses[index].subject,
+                                        style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                                          color: Colors.grey,
+                                        ),
+                                      ),
+                                      subtitle: Text(
+                                        '${sampleClasses[index].time.hour}:${sampleClasses[index].time.minute} '
+                                            '- ${sampleClasses[index].time.hour + 1}:${sampleClasses[index].time.minute}',
+                                        style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      children: [
+                                        Padding(
+                                          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                                          child: Row(
+                                            children: [
+                                              Text(
+                                                'Class: ',
+                                                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                                                  color: Colors.grey,
+                                                ),
+                                              ),
+                                              Text(
+                                                sampleClasses[index].room.toString(),
+                                                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                                                  fontWeight: FontWeight.bold,
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                        Padding(
+                                          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                                          child: Row(
+                                            children: [
+                                              Text(
+                                                'Section: ',
+                                                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                                                  color: Colors.grey,
+                                                ),
+                                              ),
+                                              Text(
+                                                sampleClasses[index].section,
+                                                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                                                  fontWeight: FontWeight.bold,
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                        Padding(
+                                          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                                          child: Row(
+                                            children: [
+                                              Text(
+                                                'Subject: ',
+                                                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                                                  color: Colors.grey,
+                                                ),
+                                              ),
+                                              Text(
+                                                sampleClasses[index].subject,
+                                                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                                                  fontWeight: FontWeight.bold,
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                        Padding(
+                                          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                                          child: Row(
+                                            children: [
+                                              Text(
+                                                'Time: ',
+                                                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                                                  color: Colors.grey,
+                                                ),
+                                              ),
+                                              Text(
+                                                sampleClasses[index].time.toString(),
+                                                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                                                  fontWeight: FontWeight.bold,
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                        Padding(
+                                          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                                          child: Row(
+                                            children: [
+                                              Text(
+                                                'Teacher: ',
+                                                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                                                  color: Colors.grey,
+                                                ),
+                                              ),
+                                              Text(
+                                                sampleClasses[index].teacher,
+                                                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                                                  fontWeight: FontWeight.bold,
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      ],
+                                    );
+                                  }
+                                ),
+                              ],
+                            )
+                          )
+                        )
+                      ],
+                    ),
+                  ),
                 ),
               ],
             ),
